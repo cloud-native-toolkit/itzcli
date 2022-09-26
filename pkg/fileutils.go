52ec727e92e1f67dd474b0e05f2b46b70ba1d204
Nathan Good <nathan.good@ibm.com>
2022-09-26 11:41:24 -0500

    Will need some cleanup love, but have the CLI now pulling the solution ZIP from the API and creating the jobs and pipeline in builder service.

--- pkg/fileutils.go
+++ pkg/fileutils.go
@@ -13,3 +13,21 @@ func ReadFile(path string) ([]byte, error) {
 	p := re.ReplaceAllString(path, ``)
 	return os.ReadFile(p)
 }
+
+func WriteFile(path string, data []byte) error {
+	// Open a new file for writing only
+	file, err := os.OpenFile(
+		path,
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0600,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
