4d629b4287c642d2151d9bfc8d2c1a52d9c863aa
Nathan Good <nathan.good@ibm.com>
2022-09-27 14:27:14 -0500

    Improved configuration import and added ability to list the configuration.

--- cmd/listConfig.go
+++ cmd/listConfig.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	logger "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.ibm.com/skol/atkcli/pkg"
 
 	"github.com/spf13/cobra"
 )
@@ -11,8 +13,19 @@ var listConfigCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists configuration from various sources",
 	Long:  `Lists configuration from various sources, including ocpnow`,
-	Run: func(cmd *cobra.Command, args []string) {
-		logger.Debugf("Importing configuration from <%s>...", &ocpnowCfg)
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ocpCfg := viper.GetStringSlice("ocpnow.configFiles")
+		if len(ocpCfg) == 0 {
+			logger.Warnf("No ocpnow configuration files found")
+			return nil
+		}
+		logger.Debugf("Listing configuration from <%s>...", ocpCfg[0])
+		project, err := pkg.LoadProject(ocpCfg[0])
+		if err != nil {
+			return err
+		}
+		err = project.Write(configureCmd.OutOrStdout())
+		return err
 	},
 }
 
