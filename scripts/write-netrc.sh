e774703fefb4ed5a0ed481b874914e54c3e90b24
Nathan Good <nathan.good@ibm.com>
2022-09-28 07:46:05 -0500

    Just documented purpose of scripts now that I am starting to accumulate more...

--- scripts/write-netrc.sh
+++ scripts/write-netrc.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+# write-netrc.sh
+#
+# The purpose of this script is to generate the .netrc file in the Travis
+# environment because it is required for "go get" to work with private modules.
+#
+# It uses the GITHUB_USER and GITHUB_TOKEN environment variables, so they need
+# to be configured correctly in the build for this script to work properly.
 
 set -eo pipefail
 
+# We only want to run this script if it is running in Travis
 if [ "${TRAVIS:-false}" == "true" ]; then
   cat > ~/.netrc <<EOF
 machine github.ibm.com
