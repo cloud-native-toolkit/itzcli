f5623fe2324f2882df9621d91f13a22ab9417a7b
Nathan Good <nathan.good@ibm.com>
2022-10-12 09:24:58 -0500

    Cleaned up some code in deploy function, adding tests next.

--- pkg/ocpnow.go
+++ pkg/ocpnow.go
@@ -3,6 +3,7 @@ package pkg
 import (
 	"fmt"
 	logger "github.com/sirupsen/logrus"
+	"github.ibm.com/skol/atkcli/internal/prompt"
 	"gopkg.in/yaml.v3"
 	"io"
 	"os"
@@ -69,10 +70,10 @@ Clusters:
 {{end}}
 `
 	tmpl, err := template.New("Project").Funcs(funcMap).Parse(consoleTemplate)
-	if err == nil {
-		tmpl.Execute(out, p)
+	if err != nil {
+		return err
 	}
-	return err
+	return tmpl.Execute(out, p)
 }
 
 func FindClusterByName(in *Project, name string) (*string, error) {
@@ -117,7 +118,7 @@ func ResolveVars(ref interface{}, cfg *ResolveVarConfig) (map[string]string, err
 
 	// Borrowing heavily from https://github.com/caarlos0/env/blob/main/env.go for
 	// the parsing code here. We're going to go through the struct that we have
-	// and, for each field, look for a tag (default "tfvar" and, if found, we will
+	// and, for each field, look for a tag (default "tfvar") and, if found, we will
 	// add both the tag's name (with the prefix) to the map coming back along with
 	// the string value representation.
 	ptr := reflect.ValueOf(ref)
@@ -156,4 +157,115 @@ func NewDefaultResolveVarConfig() *ResolveVarConfig {
 		Prefix: "TF_VAR_",
 		Tag:    "tfvar",
 	}
+}
+
+// BuildParamResolver is used to resolve missing parameters
+type BuildParamResolver struct {
+	// The project
+	project *Project
+	// The name of the cluster from the project.yaml file
+	cluster string
+	// The job parameters as required by whatever job we're going to run.
+	jobParams []JobParam
+	// The map of the cluster variables that were found in the ocpnow project
+	// config
+	clusterVars map[string]string
+	// The map of variables that come from the credentials section of the ocpnow
+	// project.yaml
+	credVars map[string]string
+	// The array of variables that we still need to ask the user
+	askVars []string
+	// The root prompt
+	rootPrompt *prompt.Prompt
+}
+
+func (r *BuildParamResolver) BuildPrompter(solution string) (*prompt.Prompt, error) {
+	// Okay, so now I have the required vars and I can now build up the prompts
+	// to ask my user for the values.
+	builder := prompt.NewPromptBuilder()
+
+	rootQuestion, err := builder.Path("proceed").
+		Text(fmt.Sprintf("This will deploy the solution %s to cluster %s; continue?", solution, r.cluster)).
+		WithOptions(prompt.YesNo()).
+		Build()
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range r.askVars {
+		logger.Tracef("Building prompt for <%s>", v)
+		subP, _ := prompt.NewPromptBuilder().
+			Path(v).
+			Textf("What value would you like to use for '%s'?", v).
+			Build()
+		rootQuestion.AddSubPrompt(subP)
+	}
+	r.rootPrompt = rootQuestion
+	return rootQuestion, nil
+}
+
+func (r *BuildParamResolver) ResolvedParams() map[string]string {
+	m := make(map[string]string)
+	for _, p := range r.jobParams {
+		envVal, ok := os.LookupEnv(p.Name)
+		if ok {
+			logger.Tracef("Using build parameter <%s> from environment with value <%s>.", p.Name, envVal)
+			m[p.Name] = fmt.Sprintf("%v", envVal)
+		}
+	}
+	for k, v := range r.rootPrompt.VarMap() {
+		logger.Tracef("Adding build parameter <%s> with value <%s>.", k, v)
+		m[k] = v
+	}
+	for k, v := range r.clusterVars {
+		logger.Tracef("Adding build parameter <%s> with value <%s>.", k, v)
+		m[k] = v
+	}
+	for k, v := range r.credVars {
+		logger.Tracef("Adding build parameter <%s> with value <%s>.", k, v)
+		m[k] = v
+	}
+	return m
+}
+
+func NewBuildParamResolver(project *Project, cluster string, params []JobParam) (*BuildParamResolver, error) {
+
+	cRef, err := FindClusterByName(project, cluster)
+	if err != nil {
+		return nil, err
+	}
+
+	cInfo := project.Clusters[*cRef]
+	clusterVars, _ := ResolveVars(&cInfo, nil)
+	logger.Debugf("Got cluster vars: %v", clusterVars)
+	logger.Debugf("Using region: %s", clusterVars["TF_VAR_region"])
+	credInfo := project.Credentials[cInfo.CredId]
+	credVars, _ := ResolveVars(&credInfo, nil)
+	logger.Debugf("Got cred vars: %v", credVars)
+
+	// Now we have a list of the required parameters (vars), and we need
+	// to look at the ones that we have and that have values (clusterVars and
+	// credVars), and also look into the os.Environment. We'll build a list
+	// of the required ones that we don't have values for so that we can
+	// prompt the user.
+
+	askVars := make([]string, 0)
+	for _, p := range params {
+		_, foundInUser := Lookup(p, clusterVars)
+		_, foundInCred := Lookup(p, credVars)
+		_, foundInEnv := os.LookupEnv(p.Name)
+		if !foundInUser && !foundInCred && !foundInEnv && len(p.Value) == 0 {
+			logger.Debugf("Found no existance of <%s>, adding to list of required vars", p.Name)
+			askVars = append(askVars, p.Name)
+		}
+	}
+
+	return &BuildParamResolver{
+		cluster:     cluster,
+		jobParams:   params,
+		clusterVars: clusterVars,
+		credVars:    credVars,
+		askVars:     askVars,
+	}, nil
 }
