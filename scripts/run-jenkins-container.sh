510ad4419b87f6e590beffb99e31631a96382873
Nathan Good <nathan.good@ibm.com>
2022-10-24 16:07:32 -0500

    Updates for fixing configuration.

--- scripts/run-jenkins-container.sh
+++ scripts/run-jenkins-container.sh
@@ -4,12 +4,14 @@ set -ou pipefail
 
 #  --name jenkins \
 # --mount type=bind,source=$(pwd)/data,target=/var/jenkins_home \
+#  -p 50000:50000 \
+#  --user 1000:1000 \
+
 podman run \
   -d \
   --privileged \
-  --user 1000:1000 \
-  -p 8080:8080 -p 50000:50000 \
+  -p 8080:8080 \
   -e JENKINS_ADMIN_ID=ngood \
   -e JENKINS_ADMIN_PASSWORD=p4ssw0rd001 \
   --mount type=bind,source=/var/data/jenkins,target=/var/jenkins_home \
-  localhost/atkci
+  quay.io/ocpnow/atkci
