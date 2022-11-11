46c9376e65e78d45c1dcbd96e62a6c21c9f5c66c
Nathan Good <nathan.good@ibm.com>
2022-11-11 13:57:30 -0600

    Added functionality to start other workspaces

---
+++ cmd/workspace.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/mitchellh/mapstructure"
+	logger "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.ibm.com/skol/atkmod"
+	"github.ibm.com/skol/itzcli/pkg"
+)
+
+// workspaceCmd represents the workspace command
+var workspaceCmd = &cobra.Command{
+	Use:    "workspace [workspace name]",
+	Short:  "Executes and interacts with different workspaces.",
+	Long:   `Executes and interacts with different workspaces.`,
+	Args:   cobra.ExactArgs(1),
+	PreRun: SetLoggingLevel,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		logger.Infof("Running command: %s", args[0])
+		workspace := args[0]
+		// First, look up the configuration for the command
+		key := pkg.Keyify(workspace)
+		serviceConfig := &pkg.ServiceConfig{}
+		err := viper.UnmarshalKey(fmt.Sprintf("%s.%s", "workspaces", key), &serviceConfig, options)
+		if err != nil {
+		}
+		logger.Tracef("Found configuration for key %s: %v", key, serviceConfig)
+
+		logger.Infof("Starting workspace: %s", workspace)
+		wsImg := &pkg.Service{
+			CfgPrefix:   key,
+			DisplayName: workspace,
+			ImgName:     serviceConfig.Image,
+			IsLocal:     serviceConfig.Local,
+			Start:       pkg.RunHandler,
+			MapToUID:    -1,
+			VolumeOpt:   serviceConfig.MountOpts,
+			Flags:       []string{"--rm", "-it"},
+		}
+		if len(serviceConfig.LocalDir) > 0 {
+			wsImg.Volumes = map[string]string{
+				serviceConfig.LocalDir: "/workspace",
+			}
+		}
+		services := []pkg.Service{*wsImg}
+		ctx := &atkmod.RunContext{
+			Out: reservationCmd.OutOrStdout(),
+			Err: reservationCmd.ErrOrStderr(),
+			In:  reservationCmd.InOrStdin(),
+		}
+		err = pkg.StartupServices(ctx, services, pkg.Sequential)
+		if err != nil {
+			logger.Error("error is: %v", err)
+		}
+		return err
+	},
+}
+
+func options(config *mapstructure.DecoderConfig) {
+	config.ErrorUnused = false
+	config.ErrorUnset = false
+}
+
+func init() {
+	RootCmd.AddCommand(workspaceCmd)
+}
