57147d09cfcf57465bd7f53a12a433a95db4672c
Craig Cooper <Craig.Cooper@ibm.com>
2023-04-19 08:18:04 -0500

    Update missing token error message

--- cmd/listReservation.go
+++ cmd/listReservation.go
@@ -44,7 +44,7 @@ func listReservations(cmd *cobra.Command, args []string) error {
 	}
 
 	if len(token) == 0 {
-		return fmt.Errorf("no API token specified for reservations")
+		return fmt.Errorf("no API token specified for reservation. Please run the auth login command to login to TechZone.")
 	}
 
 	logger.Debugf("Using API URL \"%s\" and token \"%s\" to get list of reservations...",
