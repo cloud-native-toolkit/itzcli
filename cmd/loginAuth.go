4a3760c43325df7489aa36f9e09b94838f81627c
Craig Cooper <Craig.Cooper@ibm.com>
2023-05-05 10:04:55 -0500

    handler to capture user abort command during login

--- cmd/loginAuth.go
+++ cmd/loginAuth.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+
 	"github.com/cloud-native-toolkit/itzcli/pkg"
 	"github.com/cloud-native-toolkit/itzcli/pkg/auth"
 	logger "github.com/sirupsen/logrus"
@@ -18,6 +21,13 @@ var loginCmd = &cobra.Command{
 	Long:   `Stores tokens in the configuration for the given service.`,
 	PreRun: SetLoggingLevel,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		go func() {
+			<-c
+			logger.Debugln("User exited during auth login...")
+			os.Exit(1)
+		}()
 		// Handle the legacy way of login via the text file
 		if filePath != "" {
 			return TextFileLogin(cmd, args)
