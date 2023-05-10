3d10bb92277c0ced79b9fd7587f1270b8e4e07fd
Craig Cooper <Craig.Cooper@ibm.com>
2023-05-10 13:28:35 -0500

    updates to command usage terms

--- cmd/api.go
+++ cmd/api.go
@@ -10,13 +10,8 @@ import (
 // apiCmd represents the api command
 var apiCmd = &cobra.Command{
 	Use:   "api",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Starts API server http://localhost:8795",
+	Long: `Starts API server localhost:8795. Exposes certain commands as HTTP API's.`,
 }
 
 func init() {
